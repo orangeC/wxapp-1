664c6c6062a24c2717fa7e6f12564bd425cd2be6
orangeC <18332563755@163.com>
2017-02-03 08:56:52 +0800

    a

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -1,41 +1,50 @@
+//logs.js
+var app = getApp()
 Page({
-  data:{
-    title:""
+  data: {
+    title: "请求过来的商家店名",
+    showList: [
+      { name: "帮家博士",tel:"18332563755",address:"滨海新区开发区第二大街与新城东路交口"}
+    ],
+    hidden: false,
+    movie: {},
+    point: {
+      latitude: "23.099994",
+      longitude: "113.324520"
+    },
+    markers: []
   },
-  onLoad:function(options){
-    // 生命周期函数--监听页面加载
-    String2
+  onLoad: function () {
+
   },
-  onReady:function(){
-    // 生命周期函数--监听页面初次渲染完成
-    String3
+  onReady: function () {
+    console.log("logs page execute: onReady.");
+    wx.setNavigationBarTitle({ title: this.data.title });
   },
-  onShow:function(){
-    // 生命周期函数--监听页面显示
-    String4
+  onShow: function () {
+    // Do something when page show.
+    console.log("logs page execute: onShow.");
+    var that = this;
+    setTimeout(function () {
+      that.setData({
+        hidden: true
+      });
+    }, 1500);
   },
-  onHide:function(){
-    // 生命周期函数--监听页面隐藏
-    String5
+  onHide: function () {
+    // Do something when page hide.
+    console.log("logs page execute: onHide.");
   },
-  onUnload:function(){
-    // 生命周期函数--监听页面卸载
-    String6
+  onUnload: function () {
+    // Do something when page close.
+    console.log("logs page execute: onUnload.");
   },
-  onPullDownRefresh: function() {
-    // 页面相关事件处理函数--监听用户下拉动作
-    String7
+  onPullDownRefresh: function () {
+    // Do something when pull down.
+    console.log("logs page execute: onPullDownRefresh.");
   },
-  onReachBottom: function() {
-    // 页面上拉触底事件的处理函数
-    String8
-  },
-  onShareAppMessage: function() {
-    // 用户点击右上角分享
-    return {
-      title: 'title', // 分享标题
-      desc: 'desc', // 分享描述
-      path: 'path' // 分享路径
-    }
+  onReachBottom: function () {
+    // Do something when page reach bottom.
+    console.log("logs page execute: onReachBottom.");
   }
 })
